666ed5793f11993a5d52e68b326d80ff8726e10e
prismatic <42003049+astudillom@users.noreply.github.com>
2025-06-15 01:02:59 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+    },
+    {
+        path: 'mfea',
+        loadChildren: () => loadRemoteModule('mfe-a', './RoutesA').then((m) => m.routes),
+    },
+    {
+        path: 'mfeb',
+        loadChildren: () => loadRemoteModule('mfe-b', './RoutesB').then((m) => m.routes),
+    },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Router, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatGridListModule],
+  providers: [Router],
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected title = 'shell';
+
+  constructor(private readonly router: Router) { }
+
+  navigateToMfeA() {
+    this.router.navigate(['/mfea']);
+  }
+  navigateToMfeB() {
+    this.router.navigate(['/mfeb']);
+  }
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+
 }
